63f37a0b66e39f02ba60580a49e76f47511fbef6
alessandro-geraldo <alessandro.com@hotmail.com>
2021-09-05 16:02:35 -0300

    first commit

---
+++ src/main/java/pos/simulado_bd/SimuladoBdApplication.java
@@ -0,0 +1,13 @@
+package pos.simulado_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladoBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pos/simulado_bd/SimuladoBdApplicationTests.java
@@ -0,0 +1,13 @@
+package pos.simulado_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladoBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
