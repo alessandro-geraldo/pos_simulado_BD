69d60e2f43108ddf15097bc3a15ecebfb11410de
alessandro-geraldo <alessandro.com@hotmail.com>
2021-09-05 16:23:07 -0300

    create class Cantor

---
+++ src/main/java/pos/simulado_bd/model/Cantor.java
@@ -0,0 +1,48 @@
+package pos.simulado_bd.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Cantor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codCantor;
+    @Column(nullable = false)
+    private String nomeCantor;
+    @Column(nullable = false)
+    private String pais;
+
+    public Cantor() {
+    }
+
+    public Cantor(Long codCantor, String nomeCantor, String pais) {
+        this.codCantor = codCantor;
+        this.nomeCantor = nomeCantor;
+        this.pais = pais;
+    }
+
+    public Long getCodCantor() {
+        return codCantor;
+    }
+
+    public void setCodCantor(Long codCantor) {
+        this.codCantor = codCantor;
+    }
+
+    public String getNomeCantor() {
+        return nomeCantor;
+    }
+
+    public void setNomeCantor(String nomeCantor) {
+        this.nomeCantor = nomeCantor;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
